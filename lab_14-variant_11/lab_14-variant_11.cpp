bfc350eb974d5a881c8357542afc5ce7bb8516aa
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-04-16 02:28:33 +0300

    Update

--- lab_14-variant_11/lab_14-variant_11.cpp
+++ lab_14-variant_11/lab_14-variant_11.cpp
@@ -1,20 +1,63 @@
-// lab_14-variant_11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>;
+#include "windows.h";
+using namespace std;
 
-#include <iostream>
+struct SearchResult {
+    int searchValue = -1;
+    int attemptsCount = 0;
+};
+
+SearchResult binarySearch(int n, const int const *a, int count) {
+    int low = 0;
+    int high = count - 1;
+    SearchResult searchResult;
+
+    while (low < high) {
+        searchResult.attemptsCount++;
+        int mid = low + (high - low) / 2;
+        if (a[mid] > n) {
+            high = mid - 1;
+        }
+        else if (a[mid] < n) {
+            low = mid + 1;
+        }
+        else {
+            searchResult.searchValue = mid;
+            return searchResult;
+        }
+    }
+    return searchResult;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int count = 25;
+    int a[count];
+
+    for (int i = 0; i < count; i++) {
+        if (i == 0) {
+            a[i] = 1;
+        }
+        else {
+            a[i] = a[i - 1] * 2;
+        }
+    }
+
+    unsigned int n;
+    cout << "Введіть число для пошуку: ";
+    cin >> n;
+
+    SearchResult searchResult = binarySearch(n, a, count);
+
+    if (searchResult.searchValue == -1) {
+        cout << "Число " << n << " не знайдено. ";
+    }
+    else {
+        cout << "Число " << n << " знайдено. Його індекс " << searchResult.searchValue << ". ";
+    }
+    cout << "Кількість ітерацій пошуку: " << searchResult.attemptsCount << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
